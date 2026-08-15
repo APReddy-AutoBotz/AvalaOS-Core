347d838374cbdb97aa11220df648e909ea48fa4b
Avala Parameswara Reddy <avalaparameswarareddy@gmail.com>
2026-08-15 18:55:50 +0530

    chore(hosted): keep diagnostics implementation colocated

--- tests/browser/networkSafetyDiagnostics.ts
+++
@@ -1,9 +0,0 @@
-export const INVALID_NETWORK_ORIGIN = 'invalid-url';
-
-export const diagnosticOrigin = (requestUrl: string): string => {
-  try {
-    return new URL(requestUrl).origin;
-  } catch {
-    return INVALID_NETWORK_ORIGIN;
-  }
-};
